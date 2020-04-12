46fc885a2770a3900db3d9b594c863b688176792
Sanchit Rk <sanchitrrk@gmail.com>
2020-04-12 11:21:35 +0530

    added Fizz Buzz solution

---
+++ python/FizzBuzz.py
@@ -0,0 +1,24 @@
+#
+#
+# https://leetcode.com/problems/fizz-buzz/
+#
+# Time Complexity : O(n)
+# Space Complexity: O(1)
+#
+
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        results = []
+        for i in range(1, n+1):
+            if i % 15 == 0:
+                results.append("FizzBuzz")
+            elif i % 5 == 0:
+                results.append("Buzz")
+            elif i % 3 == 0:
+                results.append("Fizz")
+            else:
+                results.append(str(i))
+
+        return results
